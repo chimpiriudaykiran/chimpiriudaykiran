6f2178cb2f9e0b89c08a0fd45413b73f21d871a9
chimpiriudaykiran <udaykiranchimpiri@my.unt.edu>
2025-04-09 14:31:54 -0500

    Fixed Sidebar

--- portfolio/components/Sidebar.tsx
+++ portfolio/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { portfolioData } from "@/data/portfolioData";
 
 const Sidebar: React.FC = () => (
-    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 hidden md:block">
+    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 hidden md:block z-50">
         <ul className="menu menu-vertical bg-base-200 p-4 rounded-r-lg shadow-lg">
             <li><a href={portfolioData.github} target="_blank" className="btn btn-ghost tooltip" data-tip="GitHub"><i className="fab fa-github"></i></a></li>
             <li><a href={portfolioData.linkedin} target="_blank" className="btn btn-ghost tooltip" data-tip="LinkedIn"><i className="fab fa-linkedin"></i></a></li>
