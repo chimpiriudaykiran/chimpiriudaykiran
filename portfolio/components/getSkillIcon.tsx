12e50fa0093a427e92fb9a2b3ef1f7bfa0879e96
chimpiriudaykiran <udaykiranchimpiri@my.unt.edu>
2025-06-21 16:45:14 -0500

    Updated icons

---
+++ portfolio/components/getSkillIcon.tsx
@@ -0,0 +1,84 @@
+import {
+  SiPython, SiCplusplus, SiJavascript, SiHtml5, SiCss3,
+  SiReact, SiNodedotjs, SiExpress, SiDjango, SiFlask, SiSpringboot,
+  SiTensorflow, SiPytorch, SiScikitlearn, SiPandas, SiNumpy, SiApachespark, SiJquery,
+  SiPostgresql, SiMongodb, SiFirebase, SiAzuredevops, SiGooglecloud,
+  SiDocker, SiKubernetes, SiJenkins, SiGit, SiTableau, SiPowerbi,
+  SiVisualstudiocode, SiEclipseide, SiApachehive, SiMysql, SiMicrosoftsqlserver,
+  SiJirasoftware
+} from "react-icons/si";
+
+import { FaDatabase, FaUserGraduate, FaJava, FaAws } from "react-icons/fa";
+import { VscAzure } from "react-icons/vsc";
+import { AiOutlineTool } from "react-icons/ai";
+import { TbBrandCpp } from "react-icons/tb";
+import { IconType } from "react-icons";
+
+const skillMap: Record<string, IconType> = {
+  "Python": SiPython,
+  "Java": FaJava,
+  "C++": SiCplusplus,
+  "C": TbBrandCpp,
+  "JavaScript": SiJavascript,
+  "SAP ABAP": FaDatabase,
+  "HTML": SiHtml5,
+  "CSS": SiCss3,
+
+  "React.js": SiReact,
+  "Node.js": SiNodedotjs,
+  "Express.js": SiExpress,
+  "Django": SiDjango,
+  "Flask": SiFlask,
+  "Spring Boot": SiSpringboot,
+  "TensorFlow": SiTensorflow,
+  "PyTorch": SiPytorch,
+  "Scikit-learn": SiScikitlearn,
+  "Pandas": SiPandas,
+  "NumPy": SiNumpy,
+  "Spark": SiApachespark,
+  "jQuery": SiJquery,
+
+  "SQL": SiMicrosoftsqlserver,
+  "MySQL": SiMysql,
+  "PostgreSQL": SiPostgresql,
+  "MongoDB": SiMongodb,
+  "Firebase": SiFirebase,
+  "Hadoop": FaDatabase,
+  "Hive": SiApachehive,
+
+  "AWS": FaAws,
+  "Azure": VscAzure,
+  "Google Cloud Platform (GCP)": SiGooglecloud,
+  "Docker": SiDocker,
+  "Kubernetes": SiKubernetes,
+  "Jenkins": SiJenkins,
+  "Git": SiGit,
+  "CI/CD": AiOutlineTool,
+
+  "JIRA": SiJirasoftware,
+  "Postman": AiOutlineTool,
+  "VS Code": SiVisualstudiocode,
+  "Eclipse": SiEclipseide,
+  "Tableau": SiTableau,
+  "Power BI": SiPowerbi,
+  "Matplotlib": AiOutlineTool,
+  "Plotly": AiOutlineTool,
+
+  "Agile Software Development": FaUserGraduate,
+  "Microsoft Azure Fundamentals": FaUserGraduate,
+
+  "Quick Learner": FaUserGraduate,
+  "Strong Decision Maker": FaUserGraduate,
+  "Complex Problem Solver": FaUserGraduate,
+  "Innovative": FaUserGraduate,
+  "Flexible": FaUserGraduate,
+
+  "GenAI": AiOutlineTool,
+  "LLMs": AiOutlineTool,
+  "NLP": AiOutlineTool
+};
+
+export const getSkillIcon = (name: string): JSX.Element => {
+  const Icon = skillMap[name];
+  return Icon ? <Icon className="inline-block mr-1" /> : <AiOutlineTool className="inline-block mr-1" />;
+};
